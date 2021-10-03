8dd55be0c2b3f67686ee5fca39e864c8ca8a6fe6
Bill Kellogg <bkellogg@epiqsystems.com>
2021-10-03 10:59:59 -0500

    +directory creation

--- pterodactyl/backup-ptero.sh
+++ pterodactyl/backup-ptero.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 cd /srv
+
+# echo "Testing to confirm /media/minecraftservers/pterodactyl directory exists..."
+# if sudo test -d /opt/ibm/notes; then
+# 	echo "$NOTES_VER" > ~/kk-updateversion
+# fi
+
+# if ! test -f ~/ibm/notes/data/user.id; then
+sudo mkdir -p /media/minecraftservers/pterodactyl
+
 tar zcf /media/minecraftservers/pterodactyl/pterodactyl_panel-`date +%Y%m%d-%H%M`.tar.gz pterodactyl
