eeab1148d9932e67aa3e2aa66e2f7e8f76721f74
Bill Kellogg <bkellogg@epiqsystems.com>
2022-06-19 11:48:04 -0500

    added new 1.19 server

--- new-serversetup.sh
+++ new-serversetup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # server guid
-serverPath="192f6c42-38cf-4548-9173-788ed50317c9"
+#serverPath="192f6c42-38cf-4548-9173-788ed50317c9" # creative world
+serverPath="d7fe4e9e-4984-4679-be9c-a36dfad62764" # 1.19
 
 # base vars
-pluginRepo="/home/billkalicious/common_plugins_117"
+#pluginRepo="/home/billkalicious/common_plugins_117"
+pluginRepo="/home/billkalicious/common_plugins_119"
 pteroVolumes="/var/lib/pterodactyl/volumes/"
 fullServerPath="$pteroVolumes$serverPath/"
 pluginPath="${fullServerPath}plugins/"
@@ -14,24 +16,44 @@ bungeeServerPath="${pteroVolumes}/a41bf189-6bea-46da-bafa-653a98d32fdc/"
 bungeeConfig="${pteroVolumes}/a41bf189-6bea-46da-bafa-653a98d32fdc/config.yml"
 
 # server specific vars
-iamFile="$fullServerPath/_IAMCREATIVE"
-gameMode="creative"
-levelName="creative_world"
-pvp="false"
-difficulty="peaceful"
+# iamFile="$fullServerPath/_IAMCREATIVE"
+# gameMode="creative"
+# levelName="creative_world"
+# pvp="false"
+# difficulty="peaceful"
+# onlineMode="false"
+# whiteList="false"
+# spawnNPCs="false"
+# spawnAnimals="false"
+# spawnMonsters="false"
+# spawnProtection="0"
+# enableCommandBlocks="true"
+# viewDistance="20"
+# allowNether="false"
+# bungeeServerName="builderworld"
+# serverAddress="192.168.1.111:25581"
+# serverMOTD="Creative Builder World"
+# serverRestricted="true"
+
+iamFile="$fullServerPath/_IAM119"
+gameMode="survival"
+levelName="world"
+pvp="true"
+difficulty="normal"
 onlineMode="false"
-whiteList="false"
-spawnNPCs="false"
-spawnAnimals="false"
-spawnMonsters="false"
+whiteList="true"
+spawnNPCs="true"
+spawnAnimals="true"
+spawnMonsters="true"
 spawnProtection="0"
 enableCommandBlocks="true"
 viewDistance="20"
-allowNether="false"
-bungeeServerName="builderworld"
-serverAddress="192.168.1.111:25581"
-serverMOTD="Creative Builder World"
-serverRestricted="true"
+allowNether="true"
+bungeeServerName="nineteen"
+serverAddress="192.168.1.111:25583"
+serverMOTD="Minecraft 1.19 World!"
+serverRestricted="false"
+levelSeed="-7820269420922620817"
 
 echo "*****************************************************************"
 echo "Variables:"
@@ -59,6 +81,7 @@ echo "bungeeServerName: $bungeeServerName"
 echo "serverAddress: $serverAddress"
 echo "serverMOTD: $serverMOTD"
 echo "serverRestricted: $serverRestricted"
+echo "levelSeed: $levelSeed"
 echo "*****************************************************************"
 echo ""
 read -n1 -r -p "Press any key to continue..." key
@@ -87,6 +110,7 @@ sed -i "s/^\(spawn-protection=\).*$/\1$spawnProtection/" $serverProperties;
 sed -i "s/^\(enable-command-block=\).*$/\1$enableCommandBlocks/" $serverProperties;
 sed -i "s/^\(view-distance=\).*$/\1$viewDistance/" $serverProperties;
 sed -i "s/^\(allow-nether=\).*$/\1$allowNether/" $serverProperties;
+sed -i "s/^\(level-seed=\).*$/\1$levelSeed/" $serverProperties;
 
 echo "Checking/adding server to bungee"
 if ! sudo grep -q "$bungeeServerName" $bungeeConfig; then
@@ -104,5 +128,3 @@ if ! sudo grep -q "$bungeeServerName" $bungeeConfig; then
 else
 	echo "Server already in bungee config."
 fi
-
-
